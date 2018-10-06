c3d6d18f47d6beb1bbd945ec97998e3c0f74a0d7
dpliakos <dimitrispl96@gmail.com>
2018-10-07 00:57:56 +0300

    remove prints

--- dude_cli.py
+++ dude_cli.py
@@ -13,18 +13,10 @@ if __name__ == '__main__':
 
     dude.install()
 
-    print('\n\nReading projects')
-    dude.readProjects()
     print('\nCreating a project')
     prj = dude.createProject(name='dpliakos', path = '/home/dpliakos/Desktop/prj')
-    # prj2 = dude.createProject(name='dpliakos2', path = '/home/dpliakos/Desktop/prj2', method='default')
-    print ('\nReading projects again')
     dude.readProjects()
 
     dude.workon('/home/dpliakos/Desktop/prj')
     test = dude.activeProject
-
-    test.addVariable('__another__', 'test')
-    # test.addVariable('__lala__', 'another test')
-    test.addVariable('__test__', 'lolo')
-    test.variablesChanged()
+    
